da978c132e11d1bf3edf07e8189529a4960fd7c7
Petar Stankovic <petar@hyperchicken.com>
2020-11-17 20:43:10 +1100

    rail line and station overlay

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ let viewDefault = new ol.View({
     zoom: 12
 })
 
+let railCorridorLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+        url: 'http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wms',
+        params: {
+            'VERSION': '1.1.1',
+            'LAYERS': 'PTV_TRAIN_CORRIDOR_CENTRELINE',
+        },
+        ratio: 1,
+        serverType: 'geoserver',
+    })
+});
+
+let trainStationLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+        url: 'http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wms',
+        params: {
+            'VERSION': '1.1.1',
+            'LAYERS': 'PTV_METRO_TRAIN_STATION',
+            //'SLD': 'https://hyperchicken.com/networkmapdev/sld/stations.sld'
+        },
+        ratio: 1,
+        serverType: 'geoserver',
+    })
+});
+
 initMap();
 
 function initMap() {
@@ -14,7 +39,9 @@ function initMap() {
                 source: new ol.source.Stamen({
                     layer: 'toner-lite',
                 })
-            })
+            }),
+            railCorridorLayer,
+            trainStationLayer
         ],
         view: viewDefault
     });
